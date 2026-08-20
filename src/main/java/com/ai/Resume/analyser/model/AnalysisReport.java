3040488863a8efe70240bf7e22fd52078856d69d
Pulkit Chawla <pulkitc191@gmail.com>
2026-08-20 16:30:26 +0530

    Fixed analysis report data mismatch

--- src/main/java/com/ai/Resume/analyser/model/AnalysisReport.java
+++ src/main/java/com/ai/Resume/analyser/model/AnalysisReport.java
@@ -1,9 +1,13 @@
 package com.ai.Resume.analyser.model;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,15 +31,24 @@ public class AnalysisReport {
 
     private String roles;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "analysis_report_pros", joinColumns = @JoinColumn(name = "analysis_report_email"))
+    @OrderColumn(name = "pros_order")
     @Column(length = 450)
     private List<String> pros;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "analysis_report_cons", joinColumns = @JoinColumn(name = "analysis_report_email"))
+    @OrderColumn(name = "cons_order")
     @Column(length = 450)
     private List<String> cons;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "analysis_report_suggestions", joinColumns = @JoinColumn(name = "analysis_report_email"))
+    @OrderColumn(name = "suggestions_order")
     @Column(length = 450)
     private List<String> suggestions;
 }
+
+
+
